4f6a1901bab7130d1cf7b6049c96a14fa611e262
bob <moha201166@gmail.com>
2022-11-24 14:48:55 +0200

    h

--- lib/core/page_models/chat_profile_screen_model.dart
+++
@@ -1,9 +0,0 @@
-import 'package:base_notifier/base_notifier.dart';
-import 'package:chat_app/core/services/api/firebase_api.dart';
-
-class ChatProfileScreenModel extends BaseNotifier {
-  var firebase = FirebaseApi();
-
-
-
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,22 @@
-import 'package:chat_app/AppTheme.dart';
-import 'package:chat_app/core/services/localization/localization.dart';
-import 'package:chat_app/pages/LoginScreen.dart';
-import 'package:firebase_core/firebase_core.dart';
+
+
+import 'package:chat_app/ChatHomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
-import 'core/services/preference/preference.dart';
-import 'core/services/theme/theme_provider.dart';
-import 'core/utils/provider_setup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  await Preference.init();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((_) {
     runApp(MyApp());
   });
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: providers,
-      child: Consumer2<AppLanguageModel, ThemeProvider>(
-        builder: (c, v, l, child) {
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            theme: AppTheme.getThemeFromThemeMode(0),
-            home: LoginScreen(),
-          );
-        },
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChatHomeScreen(),
     );
   }
 }
